b29c440bc9d557e8eddac0ac0ee8ed4fda1126ad
Peter van Doorn <pvandoorn@bunq.com>
2026-05-14 17:46:21 +0200

    updated entrypoint

--- deployment/rq-scheduler/entrypoint.sh
+++ deployment/rq-scheduler/entrypoint.sh
@@ -1,9 +1,30 @@
 #!/bin/sh
 set -e
 
-# default fallback
-HOST="localhost"
-PORT="6379"
-DB="0"
+if [ -z "$REDIS_URL" ]; then
+  echo "REDIS_URL is not set"
+  exit 1
+fi
 
-exec rqscheduler --host "$REDIS_URL"  --interval 30
+# remove scheme
+URL="${REDIS_URL#*://}"
+
+# split auth and host
+AUTH="${URL%%@*}"
+HOSTPORTDB="${URL#*@}"
+
+PASSWORD="${AUTH#*:}"
+
+HOST="${HOSTPORTDB%%:*}"
+PORT_DB="${HOSTPORTDB#*:}"
+
+PORT="${PORT_DB%%/*}"
+DB="${PORT_DB#*/}"
+DB="${DB:-0}"
+
+exec rqscheduler \
+  --host "$HOST" \
+  --port "$PORT" \
+  --db "$DB" \
+  --password "$PASSWORD" \
+  --interval 30
