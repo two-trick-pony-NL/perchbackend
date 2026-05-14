6ccd9d0c91e72e9c83473a8c1671a2553f0125b8
Peter van Doorn <pvandoorn@bunq.com>
2026-05-14 17:29:20 +0200

    simplified

--- deployment/rq-scheduler/entrypoint.sh
+++ deployment/rq-scheduler/entrypoint.sh
@@ -6,12 +6,4 @@ HOST="localhost"
 PORT="6379"
 DB="0"
 
-if [ -n "$REDIS_URL" ]; then
-  # supports redis://host:port/db
-  # example: redis://redis:6379/0
-  HOST=$(echo "$REDIS_URL" | sed -E 's#redis://([^:/]+):[0-9]+/.*#\1#')
-  PORT=$(echo "$REDIS_URL" | sed -E 's#redis://[^:/]+:([0-9]+)/.*#\1#')
-  DB=$(echo "$REDIS_URL" | sed -E 's#redis://[^:/]+:[0-9]+/([0-9]+)#\1#')
-fi
-
-exec rqscheduler --host "$HOST" --port "$PORT" --db "$DB" --interval 30
+exec rqscheduler --host "$REDIS_URL"  --interval 30
